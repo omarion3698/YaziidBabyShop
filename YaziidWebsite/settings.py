b54993c712a8e085669444dd64c1d822b1623a26
Omar Hussein <omaribinbakarivic@gmail.com>
2022-08-27 15:32:17 +0300

    Automatic configuration with django-heroku

--- YaziidWebsite/settings.py
+++ YaziidWebsite/settings.py
@@ -132,3 +132,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
