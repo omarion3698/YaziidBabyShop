97d70a1c9949a7ee92a9045076b9bd782db2ae67
Omar Hussein <omaribinbakarivic@gmail.com>
2022-08-28 21:39:38 +0300

    debug

--- shop/forms.py
+++ shop/forms.py
@@ -1,5 +1,5 @@
 from dataclasses import field
-from socket import fromshare
+# from socket import fromshare
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
